4eb079bdb66d1e949c38eed61fec0744cf34fa07
yudu-dev <disney.nickname@gmail.com>
2022-05-31 20:29:51 +0300

    some fixes

--- src/main/java/com/exchange/rates/client/RatesClient.java
+++ src/main/java/com/exchange/rates/client/RatesClient.java
@@ -5,9 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
-
 
 
 @FeignClient(name = "ratesData", url = "${rates.url}")
